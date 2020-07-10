bc20bc360390d109aa08c3fe190fcbdb5016cbbd
lamakhudJHB <67950727+lamakhudJHB@users.noreply.github.com>
2020-07-10 12:35:48 +0200

    Add files via upload

---
+++ ex03/ft_is_negative.c
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_is_negative.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lamakhud <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/07/10 12:27:06 by lamakhud          #+#    #+#             */
+/*   Updated: 2020/07/10 12:31:42 by lamakhud         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+void ft_putchar(int n){
+	char is_negative;
+	if(n>0)
+		is_negative = 'P';
+	else
+		is_negative = 'N';
+}
+void ft_is_negative(int n){
+	ft_is_negative(ft_putchar);
+}
+
